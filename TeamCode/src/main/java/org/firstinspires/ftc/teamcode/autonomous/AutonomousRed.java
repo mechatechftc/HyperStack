38373c7b6143782667c9ae709ad77f439acee96d
Richik SC <sc.richik@gmail.com>
2017-11-11 17:41:59 -0600

    wip semi-working state

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousRed.java
@@ -1,17 +1,17 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.edinaftc.ninevolt.core.hw.drivetrain.Movement;
 import com.edinaftc.ninevolt.util.ExceptionHandling;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.HSRobot;
 import org.firstinspires.ftc.teamcode.functions.Elevator;
 import org.firstinspires.ftc.teamcode.functions.Gripper;
 import org.firstinspires.ftc.teamcode.functions.Tollbooth;
 import org.firstinspires.ftc.teamcode.util.StepNotifier;
 
-@Autonomous(name = "Main Autonomous", group = "real")
-public class AutonomousMain extends LinearOpMode {
+@Autonomous(name = "Red Autonomous", group = "real")
+public class AutonomousRed extends LinearOpMode {
   private HSRobot robot;
   private Movement movement;
   private Gripper gripper;
@@ -36,6 +36,7 @@ public class AutonomousMain extends LinearOpMode {
       // Perform autonomous
       gripper.grip();
       elevator.elevate(4);
+      notifier.notifyStep();
       sleep(1000);
       bumpJewel();                  // Read jewel color and knock appropriately.
       moveToGlyphBox();             // Move to cryptoboxes to deposit glyph.
@@ -60,29 +61,42 @@ public class AutonomousMain extends LinearOpMode {
         "Tollbooth lowered",
         "Gripper released",
         "Color checked",
-        "Elevator down",
-        "Gripper grip",
         "Bump jewel",
+        "Re-align",
         "Raise tollbooth",
         "Move off platform"
     }, this);
+
+    while (!robot.getHardware().imu.isGyroCalibrated() && opModeIsActive()) {
+      telemetry.addData("Gyro", "Calibrating");
+      telemetry.update();
+      idle();
+    }
+
   }
 
   private void bumpJewel() throws Exception {
     tollbooth.lower(); // Lower tollbooth arm
+    notifier.notifyStep();
     sleep(3000);
-    if (tollbooth.checkColor() == Tollbooth.JewelColor.BLUE) {
-      movement.rotate(-15, 0.2f);
-      sleep(5000);
-      tollbooth.raise();
-      sleep(5000);
+    Tollbooth.JewelColor color = tollbooth.checkColor();
+    notifier.notifyStep();
+    if (color == Tollbooth.JewelColor.BLUE) {
       movement.rotate(15, 0.2f);
-    } else if (tollbooth.checkColor() == Tollbooth.JewelColor.RED) {
-      movement.rotate(15, 0.2f);
-      sleep(5000);
+      notifier.notifyStep();
+      sleep(3000);
       tollbooth.raise();
-      sleep(5000);
+      sleep(3000);
       movement.rotate(-15, 0.2f);
+      sleep(3000);
+    } else if (color == Tollbooth.JewelColor.RED) {
+      movement.rotate(-15, 0.2f);
+      notifier.notifyStep();
+      sleep(3000);
+      tollbooth.raise();
+      sleep(3000);
+      movement.rotate(15, 0.2f);
+      sleep(3000);
     } else {
       telemetry.addLine("Error with color sensor readings");
       telemetry.update();
@@ -92,7 +106,10 @@ public class AutonomousMain extends LinearOpMode {
   private void moveToGlyphBox() throws Exception {
     movement.yDrive(-41, 0.5f);
     sleep(1000);
-    movement.rotate(-90, 0.2);
+    movement.directDrive(0f,0f,0f);
+    sleep(1000);
+    movement.rotate(-90, 0.5f);
+    sleep(1000);
   }
 
   private void releaseGlyph() {
