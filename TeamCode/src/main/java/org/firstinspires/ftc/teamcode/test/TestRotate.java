d48f870e1525b29bb43c867e37eaf311979c8763
NickD <nikolad21889@isd273.org>
2017-11-30 18:18:14 -0600

    HI RICHIK

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TestRotate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TestRotate.java
@@ -4,7 +4,6 @@ import com.edinaftc.ninevolt.core.hw.Hardware;
 import com.edinaftc.ninevolt.core.hw.drivetrain.Movement;
 import com.edinaftc.ninevolt.util.ExceptionHandling;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.HSRobot;
 
