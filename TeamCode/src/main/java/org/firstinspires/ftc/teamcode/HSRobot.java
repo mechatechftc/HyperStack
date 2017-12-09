6481372e9989fc2eb30aa0e4acae725911ec0fde
Richik SC <sc.richik@gmail.com>
2017-12-09 09:36:24 -0600

    Optimize imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HSRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HSRobot.java
@@ -1,17 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.edinaftc.ninevolt.Ninevolt;
+import com.edinaftc.ninevolt.core.hw.Hardware;
+import com.edinaftc.ninevolt.core.hw.HardwareBuilder;
 import com.edinaftc.ninevolt.core.hw.drivetrain.MecanumMovement;
+import com.edinaftc.ninevolt.core.hw.drivetrain.Movement;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.edinaftc.ninevolt.core.hw.Hardware;
-import com.edinaftc.ninevolt.core.hw.HardwareBuilder;
-import com.edinaftc.ninevolt.core.hw.drivetrain.Movement;
 import org.firstinspires.ftc.teamcode.functions.Elevator;
 import org.firstinspires.ftc.teamcode.functions.Gripper;
 import org.firstinspires.ftc.teamcode.functions.Tollbooth;
