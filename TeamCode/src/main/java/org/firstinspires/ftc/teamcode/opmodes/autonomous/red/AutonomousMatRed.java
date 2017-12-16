9a4de80f027ff221638dcd5ef0fcc2e4f3330f64
NickD <nikolad21889@isd273.org>
2017-12-16 12:36:24 -0600

    Pictograph Red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/red/AutonomousMatRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/red/AutonomousMatRed.java
@@ -7,18 +7,18 @@ import org.firstinspires.ftc.teamcode.functions.Tollbooth;
 @Autonomous(name = "Red Autonomous Mat Side", group = "real.red")
 public class AutonomousMatRed extends AutonomousMat {
 
-  private double centerDist = -38 + getPictographDist();
+  private double centerDist = -38 - getPictographDist();
 
   protected Tollbooth.JewelColor getAllianceColor() {
     return Tollbooth.JewelColor.RED;
   }
 
   protected double getYDistLeft() {
-    return centerDist + offset;
+    return centerDist - offset;
   }
 
   protected double getYDistRight() {
-    return centerDist - offset;
+    return centerDist + offset;
   }
 
   protected double getYDistCenter() {
@@ -30,6 +30,6 @@ public class AutonomousMatRed extends AutonomousMat {
   }
 
   protected double getRotationAngle() {
-    return -80;
+    return -83;
   }
 }
