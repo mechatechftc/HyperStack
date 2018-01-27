29cd73cbca90958af7ea40c20acfb0f943edf036
NickD <nikolad21889@isd273.org>
2018-01-27 10:41:44 -0600

    created dogecv test class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TestDogeCV.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.detectors.CryptoboxDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Test dogecv", group = "test")
+public class TestDogeCV extends LinearOpMode{
+
+  private CryptoboxDetector cryptoboxDetector;
+
+  public void runOpMode() {
+    customInit();
+    waitForStart();
+    detectCryptobox();
+    cryptoboxDetector.disable();
+  }
+
+  private void customInit() {
+    cryptoboxDetector = new CryptoboxDetector();
+    cryptoboxDetector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+
+    cryptoboxDetector.rotateMat = true;
+    cryptoboxDetector.enable();
+
+    telemetry.addData("Init", "Done");
+    telemetry.update();
+  }
+
+  private void detectCryptobox() {
+    while(opModeIsActive()) {
+      telemetry.addData("isCryptoBoxDetected", cryptoboxDetector.isCryptoBoxDetected());
+      telemetry.addData("isColumnDetected ", cryptoboxDetector.isColumnDetected());
+
+      telemetry.addData("Column Left ", cryptoboxDetector.getCryptoBoxLeftPosition());
+      telemetry.addData("Column Center ", cryptoboxDetector.getCryptoBoxCenterPosition());
+      telemetry.addData("Column Right ", cryptoboxDetector.getCryptoBoxRightPosition());
+      telemetry.update();
+    }
+  }
+}
