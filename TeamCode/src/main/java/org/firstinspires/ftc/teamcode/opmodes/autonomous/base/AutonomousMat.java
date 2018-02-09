4d316f18162f492164d1ae77e656e85698db2591
Richik SC <sc.richik@gmail.com>
2018-02-09 16:29:10 -0600

    Make moveToPictograph use power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/base/AutonomousMat.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/base/AutonomousMat.java
@@ -23,7 +23,7 @@ public abstract class AutonomousMat extends LinearOpMode {
   private Gripper gripper;
   private Tollbooth tollbooth;
   private Elevator elevator;
-  private final static float power = 0.25f;
+  private final static float power = 0.5f;
 
   private CloseableVuforiaLocalizer vuforia;
   private VuforiaTrackable relicTemplate;
@@ -111,7 +111,7 @@ public abstract class AutonomousMat extends LinearOpMode {
 
   private void moveToPictograph() throws Exception {
     sleep(250);
-    movement.yDrive(getPictographDist(), 0.2f);
+    movement.yDrive(getPictographDist(), power);
     sleep(500);
   }
 
